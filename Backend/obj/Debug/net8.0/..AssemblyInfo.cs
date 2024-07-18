31ab51317ee62490c8296f78a8e619af7eed77d2
mololuwa10 <mololuwasegi@yahoo.com>
2024-07-18 16:20:04 +0100

    added-ignore-file

--- Backend/obj/Debug/net8.0/..AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac3920db4156919350a91c85133a22ba7d4c041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa75022f05e623e672cd6647dce98f434631cfe3")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
