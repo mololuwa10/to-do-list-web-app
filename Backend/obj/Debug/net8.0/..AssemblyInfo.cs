8c2e6cee5f5f1aea0ed48f539a87351e81892961
mololuwa10 <mololuwasegi@yahoo.com>
2024-07-22 21:51:15 +0100

    debug-tool

--- Backend/obj/Debug/net8.0/..AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebecc1883de9d68be7e2451181c8d76152ade82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ffb273d11998b4d13187368647344e933c74f9")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
