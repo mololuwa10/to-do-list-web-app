8eb4c5e41164b6b446baa2efa188fb43506834d7
mololuwa10 <mololuwasegi@yahoo.com>
2024-08-22 23:18:13 +0100

    "Updated assembly version and rebuilt binaries"

--- Backend/obj/Debug/net8.0/..AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eee7a65df3bc2b85cbc9e214b3917770a9a1bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c93d3a2763f919db13a76fb282ce8274dc558f")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
