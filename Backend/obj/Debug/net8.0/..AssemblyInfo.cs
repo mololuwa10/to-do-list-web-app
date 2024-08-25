d14d6049b7550ccee4dd20de601e7ad113ebae3c
mololuwa10 <mololuwasegi@yahoo.com>
2024-08-25 18:55:12 +0100

    random

--- Backend/obj/Debug/net8.0/..AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b77a4c69e7715a8802be5f295adc5eb2f8dd66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7deaec35bcf65f81d3eecaaf5bd83acd9ae7ade8")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
